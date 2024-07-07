8d926c4770e81ca2aca662319fc3de85295a67ab
duke107 <sendebopriyo10@gmail.com>
2024-07-07 13:02:56 +0530

    second commmit

---
+++ myscripts.js
@@ -0,0 +1,30 @@
+// alert("ehlo0");
+// console.log("hello");
+// let c=20;
+// console.log(c);
+// let arr=[20,40,32];
+
+// let arr2=[20,40,32];
+// arr2.reverse();
+// arr2[0]=100;
+// arr.splice(0,1);
+// console.log(arr)
+// console.log(arr2)
+const arr=[2,4,6,8,10];
+
+// arr.find((value,index)=>{
+//     if(value===6)
+//     {
+//         console.log(value);
+//     }
+// });
+
+// const result= arr.filter((value,index)=>{
+//     if(value>4)
+//     {
+//         return value;
+//     }
+// });
+
+
+console.log(result)
